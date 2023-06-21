82b810292c80334da1e369fba2c0dc7c18cf59c9
Oliver Cray <128925359+OliverCray@users.noreply.github.com>
2023-06-21 13:11:15 +0100

    Corrected naming of elements in test()

--- tests/shapes.test.js
+++ tests/shapes.test.js
@@ -1,7 +1,7 @@
 const { Circle, Square, Triangle } = require('../lib/shapes')
 
 describe('Triangle', () => {
-    test('should render svg for a blue triangle', () => {
+    test('should render svg for a blue polygon element', () => {
         const expectedSVG = '<polygon points="150, 18 244, 182 56, 182" fill="blue"/>'
         const triangle = new Triangle()
         triangle.setColour('blue')
@@ -11,7 +11,7 @@ describe('Triangle', () => {
 })
 
 describe('Circle', () => {
-    test('should render svg for a cyan circle', () => {
+    test('should render svg for a cyan circle element', () => {
         const expectedSVG = '<circle cx="150" cy="100" r="80" fill="cyan"/>'
         const circle = new Circle()
         circle.setColour('cyan')
@@ -21,7 +21,7 @@ describe('Circle', () => {
 })
 
 describe('Square', () => {
-    test('should render svg for a bisque square', () => {
+    test('should render svg for a bisque rect element', () => {
         const expectedSVG = '<rect x="90" y="40" width="120" height="120" fill="bisque"/>'
         const square = new Square()
         square.setColour('bisque')
